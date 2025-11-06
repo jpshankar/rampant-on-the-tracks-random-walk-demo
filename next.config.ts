0921e38f46907e719ec37a5cf324afad1d3fc002
Javas Shankar <javasshankar@gmail.com>
2025-11-05 20:06:36 -0500

    - Adding deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: "/rampant-on-the-tracks-random-walk-demo"
 };
 
 export default nextConfig;
