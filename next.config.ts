3c38dc87c66d4d17b0aaaaf76c9dfc80d9fe8620
Javas Shankar <javasshankar@gmail.com>
2025-11-06 11:25:33 -0500

    Updates

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: "/rampant-on-the-tracks-random-walk-demo"
-};
-
-export default nextConfig;
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Grid from '@/components/grid/index';
-
-export default function Home() {
-  return <div className="pl-[35vw] pt-[20vh] flex flex-col whitespace-pre-wrap"><Grid/></div>;
-}
